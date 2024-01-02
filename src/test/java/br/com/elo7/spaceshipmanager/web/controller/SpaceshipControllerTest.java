63bc2206c5dfe717bf4af288df67f595f3c8bed5
Augusto Nunes <augustonunes.dev@gmail.com>
2024-01-02 20:14:38 +0000

    remove unused imports

--- src/test/java/br/com/elo7/spaceshipmanager/web/controller/SpaceshipControllerTest.java
+++ src/test/java/br/com/elo7/spaceshipmanager/web/controller/SpaceshipControllerTest.java
@@ -1,31 +1,29 @@
 package br.com.elo7.spaceshipmanager.web.controller;
 
-import br.com.elo7.spaceshipmanager.service.SpaceshipService;
-import br.com.elo7.spaceshipmanager.web.model.NewSpaceshipDTO;
-import br.com.elo7.spaceshipmanager.web.model.PlanetDTO;
-import br.com.elo7.spaceshipmanager.web.model.SpaceshipDTO;
-import br.com.elo7.spaceshipmanager.web.model.SpaceshipInputDTO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import br.com.elo7.spaceshipmanager.service.SpaceshipService;
+import br.com.elo7.spaceshipmanager.web.model.NewSpaceshipDTO;
+import br.com.elo7.spaceshipmanager.web.model.SpaceshipDTO;
+import br.com.elo7.spaceshipmanager.web.model.SpaceshipInputDTO;
 
 @WebMvcTest(SpaceshipController.class)
 class SpaceshipControllerTest {
